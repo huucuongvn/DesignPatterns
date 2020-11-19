f93339df6dd96c63a8fb992d27eab82959d5c3d0
Cuong VN <huucuongbeat95@gmail.com>
2020-11-19 19:48:03 +0700

    Design Pattern

---
+++ DesignPatternDemo/main4.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+
+using namespace std;
+
+enum class Relationship {
+    parent,
+    child,
+    sibling,
+};
+
+struct Person {
+    string name;
+};
+
+struct RelationshipBrowser {
+    virtual vector<Person> find_all_children_of(const string &name) = 0;
+};
+
+struct Relationships : RelationshipBrowser { //low-level
+    vector<tuple<Person, Relationship, Person>> relations;
+    void add_parent_and_child(const Person &parent, const Person &child) {
+        relations.push_back({parent, Relationship::parent, child});
+        relations.push_back({child, Relationship::child, parent});
+    }
+    vector<Person> find_all_children_of(const string &name) override {
+        vector<Person> result;
+        for(auto &&[first, rel, second]:relations) {
+            if(first.name == "John" && rel == Relationship::parent) {
+                result.push_back(second);
+            }
+        }
+            return result;
+    }
+
+};
+
+struct Research {//high-level
+//    Research(Relationships &relationships) {
+        Research(RelationshipBrowser &browser) {
+            for(auto &child:browser.find_all_children_of("John")) {
+                cout << "John has a child called " << child.name << endl;
+            }
+        }
+//        auto &relations=relationships.relations;
+//        for(auto &&[first, rel, second]:relations) {
+//            if(first.name == "John" && rel == Relationship::parent) {
+//                cout << "John has a child called " << second.name << endl;
+//            }
+//        }
+//    }
+};
+
+int main()
+{
+    Person parent{"John"};
+    Person child1{"Chris"}, child2{"Matt"};
+    Relationships relationships;
+    relationships.add_parent_and_child(parent,child1);
+    relationships.add_parent_and_child(parent,child2);
+    Research _(relationships);
+    return 0;
+}
