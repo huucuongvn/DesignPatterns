9b63878d70453ab4947a773cf822a8668235d635
Cuong VN <huucuongbeat95@gmail.com>
2020-12-10 23:56:24 +0700

    handle a complex object

---
+++ Builder/BuilderFacets/PersonAddressBuilder.h
@@ -0,0 +1,24 @@
+#ifndef PERSONADDRESSBUILDER_H
+#define PERSONADDRESSBUILDER_H
+#include <string>
+
+class PersonAddressBuilder : public PersonBuilderBase {
+    typedef PersonAddressBuilder Self;
+public:
+    PersonAddressBuilder(Person &person) : PersonBuilderBase(person) {}
+    Self& at(std::string street_address) {
+        person.street_address = street_address;
+        return *this;
+    }
+    Self& with_postcode(std::string post_code) {
+        person.post_code = post_code;
+        return *this;
+    }
+    Self& in(std::string city) {
+        person.city = city;
+        return *this;
+    }
+
+};
+
+#endif // PERSONADDRESSBUILDER_H
