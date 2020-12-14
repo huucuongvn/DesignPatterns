79e5b68f454ed7511162c8d10f7d6b23cd0d1a1e
Cuong VN <huucuongbeat95@gmail.com>
2020-12-14 16:38:25 +0700

    use inheritance allow polymorphically call on the different factory

---
+++ Factories/AbstractFactory/main.cpp
@@ -0,0 +1,32 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <memory>
+#include <cmath>
+using namespace std;
+
+#include "HotDrinkFactory.h"
+#include "DrinkFactory.h"
+unique_ptr<HotDrink> make_drink(string type) {
+    unique_ptr<HotDrink> drink;
+    if(type=="tea") {
+        drink=make_unique<Tea>();
+        drink->prepare(200);
+    } else {
+        drink=make_unique<Coffee>();
+        drink->prepare(50);
+    }
+    return drink;
+}
+
+int main()
+{
+    DrinkFactory df;
+    auto c=df.make_drink("coffee");
+    return 0;
+}
