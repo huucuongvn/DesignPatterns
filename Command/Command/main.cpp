f4e921d1b9a438db9b4a1116d8bfe5cd6f3bd9be
huucuongvn <huucuongbeat95@gmail.com>
2021-07-07 15:38:39 +0700

    Command

---
+++ Command/Command/main.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct BankAccount
+{
+  int balance = 0;
+  int overdraft_limit = -500;
+
+  void deposit(int amount)
+  {
+    balance += amount;
+    cout << "deposited " << amount << ", balance now " <<
+      balance << "\n";
+  }
+
+  void withdraw(int amount)
+  {
+    if (balance - amount >= overdraft_limit)
+    {
+      balance -= amount;
+      cout << "withdrew " << amount << ", balance now " <<
+        balance << "\n";
+    }
+  }
+};
+
+struct Command
+{
+  virtual ~Command() = default;
+  virtual void call() const = 0;
+  virtual void undo() const = 0;
+};
+
+
+struct BankAccountCommand : Command
+{
+  BankAccount& account;
+  enum Action { deposit, withdraw } action;
+  int amount;
+
+  BankAccountCommand(BankAccount& account,
+    const Action action, const int amount)
+    : account(account), action(action), amount(amount) {}
+
+  void call() const override
+  {
+    switch (action)
+    {
+    case deposit:
+      account.deposit(amount);
+      break;
+    case withdraw:
+      account.withdraw(amount);
+      break;
+    default: break;
+    }
+  }
+
+  void undo() const override
+  {
+    switch (action)
+    {
+    case withdraw:
+      account.deposit(amount);
+      break;
+    case deposit:
+      account.withdraw(amount);
+      break;
+    default: break;
+    }
+  }
+};
+
+
+struct CompositeBankAccountCommand
+  : vector<BankAccountCommand>, Command
+{
+  CompositeBankAccountCommand(const initializer_list<value_type>& items)
+    : vector<BankAccountCommand>(items) {}
+
+  void call() const override
+  {
+    for (auto& cmd : *this)
+      cmd.call();
+  }
+
+  void undo() const override
+  {
+    for (auto& cmd : *this)
+      cmd.undo();
+  }
+};
+
+int main()
+{
+  BankAccount ba;
+
+  CompositeBankAccountCommand commands {
+    BankAccountCommand{ba, BankAccountCommand::deposit, 100},
+    BankAccountCommand{ba, BankAccountCommand::withdraw, 200}
+  };
+
+  cout << ba.balance << endl;
+
+
+  commands.call();
+
+  cout << ba.balance << endl;
+
+
+  commands.undo();
+
+  cout << ba.balance << endl;
+
+  getchar();
+  return 0;
+}
